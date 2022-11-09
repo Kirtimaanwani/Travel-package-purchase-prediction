b7719364999114128610cc8ad1f58986e452285c
Kirtimaanwani <sushilsw111@gmail.com>
2022-11-09 16:42:54 +0530

    Update

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0" # update as per your need
 
-REPO_NAME = "<YOUR REPO NAME>"
-AUTHOR_USER_NAME = "<YOUR USERNAME>"
-SRC_REPO = "<YOUR REPO NAME>"
-AUTHOR_EMAIL = "<YOUR EMAIL ID>"
+REPO_NAME = "Travel-package-purchase-prediction"
+AUTHOR_USER_NAME = "Kirtimaanwani"
+SRC_REPO = "travel"
+AUTHOR_EMAIL = "sushilsw111@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
