ba39202c1fb0799c8a0b7c04118f0d9146630629
Kirtimaanwani <sushilsw111@gmail.com>
2022-11-20 12:19:48 +0530

    data_ingestion config and artifact defined

---
+++ src/travel/entity/config_entity.py
@@ -0,0 +1,35 @@
+import os
+from datetime import datetime
+from src.travel.constant import training_pipeline
+from src.travel.logger import logging
+
+
+class TrainingPipelineConfig:
+
+    def __init__(self, timestamp=datetime.now()):
+        timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+        self.pipeline_name: str = training_pipeline.PIPELINE_NAME
+        self.artifact_dir: str = os.path.join(training_pipeline.ARTIFACT_DIR, timestamp)
+        self.timestamp: str = timestamp
+
+
+class DataIngestionConfig:
+    
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        logging.info("Creating DataIngestionConfig")
+
+        self.data_ingestion_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir,
+            training_pipeline.DATA_INGESTION_DIR_NAME
+        )
+
+        self.feature_store_file_path: str = os.path.join(
+            self.data_ingestion_dir,
+            training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR,
+            training_pipeline.FILE_NAME
+        )
+        
+        self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
+
+        self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
+
