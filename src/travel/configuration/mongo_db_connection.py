14a924aed450b0cf9a39260a7af07a8f0f846a95
Kirtimaanwani <sushilsw111@gmail.com>
2022-11-20 11:59:38 +0530

    trying to build connection with mongoDB

---
+++ src/travel/configuration/mongo_db_connection.py
@@ -0,0 +1,18 @@
+import pymongo
+from src.travel.constant.training_pipeline import DATA_INGESTION_DATABASE_NAME
+import certifi
+ca = certifi.where()
+
+from credential.CREDENTIAL import MONGO_DB_URL
+
+class MongoDBClient:
+    client = None
+    def __init__(self, database_name=DATA_INGESTION_DATABASE_NAME) -> None:
+        try:
+            if MongoDBClient.client is None:
+                MongoDBClient.client = pymongo.MongoClient(MONGO_DB_URL, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+        except Exception as e:
+            raise 
