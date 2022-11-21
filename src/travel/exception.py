2c673e56454d11c1e5800ca31c41832e97f9c85c
Kirtimaanwani <sushilsw111@gmail.com>
2022-11-21 17:11:46 +0530

    update

--- src/travel/exception.py
+++ src/travel/exception.py
@@ -1,6 +1,6 @@
 import sys, os
 
-def error_message_detail(error, error_detail:sys):
+def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
 
     file_name = exc_tb.tb_frame.f_code.co_filename
