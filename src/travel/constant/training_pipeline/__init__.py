8675567af7f542b2f37f8da9648e84159c1ef186
Kirtimaanwani <sushilsw111@gmail.com>
2022-12-24 12:36:48 +0530

    Update in data ingestion for train test split

--- src/travel/constant/training_pipeline/__init__.py
+++ src/travel/constant/training_pipeline/__init__.py
@@ -4,7 +4,8 @@ TARGET_FEATURE :str = "ProdTaken"
 FILE_NAME: str = "travel.csv"
 ARTIFACT_DIR: str = "artifact"
 PIPELINE_NAME: str = "travel"
-
+TRAIN_FILE_NAME:str = "train.csv"
+TEST_FILE_NAME:str = "test.csv"
 
 """ 
 Data Ingestion related constant start with DATA_INGESTION variable Name
@@ -13,6 +14,8 @@ DATA_INGESTION_DATABASE_NAME:str = "finance1"
 DATA_INGESTION_COLLECTION_NAME:str = "data_sample" # same as ""from sensor.constant.database import COLLECTION_NAME""
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2  # test_size
 
 
 
